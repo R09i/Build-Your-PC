72ccccb213b18ef53f98a49b3bff3cb11cbad459
R09i <sarkararindam2003@gmail.com>
2024-11-23 01:04:31 +0530

    git-proj1

---
+++ byPc.js
@@ -0,0 +1,27 @@
+const loginForm = document.getElementById('form');
+const loginBtn = document.getElementById('button');
+
+const box= document.querySelector('.dv2');
+
+
+loginBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  console.log("hello")
+
+  box.style.display="block"
+  const Email = document.getElementById('email').value;
+  const Password = document.getElementById('password').value;
+
+  // Validate username and password
+  if (Email === '' || Password === '') {
+    alert('Please fill in both Email and Password');
+    return;
+  }
+
+  // Call login API or authenticate user
+  console.log(`Logging in with email: ${email} and password: ${password}`);
+
+  // Redirect to dashboard or next page
+  window.location.href = 'home.html';
+});
+
